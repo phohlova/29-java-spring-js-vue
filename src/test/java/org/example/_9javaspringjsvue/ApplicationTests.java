2af7dcd8c59c019d46c6be53691a0b94442ce6b7
phohlova <hohlova.pv@edu.spbstu.ru>
2026-02-17 10:36:20 +0300

    init commit

---
+++ src/test/java/org/example/_9javaspringjsvue/ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example._9javaspringjsvue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
