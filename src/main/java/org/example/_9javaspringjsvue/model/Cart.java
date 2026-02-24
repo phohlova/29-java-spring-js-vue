4cd136aa80f091d9a32f2f12f22e8e0fdfd6ff67
phohlova <hohlova.pv@edu.spbstu.ru>
2026-02-24 10:33:41 +0300

    feature: создание новый сущностей БД: users, carts

---
+++ src/main/java/org/example/_9javaspringjsvue/model/Cart.java
@@ -0,0 +1,17 @@
+package org.example._9javaspringjsvue.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "carts")
+public class Cart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "user_id", nullable = false, unique = true)
+    @JsonIgnore
+    private User user;
+}
---
+++ src/main/java/org/example/_9javaspringjsvue/model/User.java
@@ -0,0 +1,39 @@
+package org.example._9javaspringjsvue.model;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+
+import java.time.ZonedDateTime;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name", nullable = false, length = 150)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false, length = 150)
+    private String lastName;
+
+    @Column(nullable = false, unique = true, length = 255)
+    private String email;
+
+    @Column(name = "password_hash", nullable = false)
+    private String passwordHash;
+
+    @Column(nullable = false)
+    private String role;
+
+    @Column(name = "created_at")
+    private ZonedDateTime createdAt;
+
+    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
+    @JsonIgnore
+    private Cart cart;
+
+
+}
